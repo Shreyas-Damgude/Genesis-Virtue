207d328b42cfe39fc74f4347285e606bd46f10b8
Shreyas-Damgude <shreyasdamgude@gmail.com>
2024-02-18 11:50:43 +0530

    Initial Commit

--- app/js/emailSubmit.js
+++ app/js/emailSubmit.js
@@ -6,9 +6,37 @@ sendBtn.addEventListener("click", function (e) {
   e.preventDefault();
   const email = input.value;
 
-  email
-    ? check.test(email)
-      ? window.alert("You'll be updated with news, tips & tricks")
-      : window.alert("Enter valid email")
-    : window.alert("Enter your email");
+  if (!email) {
+    window.alert("Enter your email");
+    return;
+  }
+
+  if (!check.test(email)) {
+    window.alert("Enter valid email");
+    return;
+  }
+  sendEmail();
+
+  input.textContent = "";
 });
+
+function sendEmail() {
+  fetch("../../mail.php", {
+    method: "POST", // Adjust method if needed (GET, PUT, etc.)
+    body: JSON.stringify({
+      // Optional data to send to PHP
+      name: "John Doe",
+      email: "johndoe@example.com",
+      message: "Hello from JavaScript!",
+    }),
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      // Handle response from PHP, e.g., display success message
+      console.log(data);
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error(error);
+    });
+}
