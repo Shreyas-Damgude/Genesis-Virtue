c239d7c7c57e16bc6ae19e40851ef9282fe9abc9
Shreyas-Damgude <shreyasdamgude@gmail.com>
2024-02-19 21:57:10 +0530

    Changes

--- app/js/submit.js
+++ app/js/submit.js
@@ -1,28 +1,8 @@
 const submitBtn = document.querySelector(".submit");
-const subscribeForm = document.querySelector(".form");
 const detailsForm = document.querySelector(".form-box");
-const subscribeBtn = document.querySelector(".subscribe");
 
 const check = /@gmail.com/;
 const info = [];
-
-if (subscribeBtn)
-  subscribeBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    const dataArray = [...new FormData(subscribeForm)];
-    const data = Object.fromEntries(dataArray);
-    const email = data.email;
-
-    email
-      ? check.test(email)
-        ? window.alert("You will be updated with news, tips and tricks")
-        : window.alert("Enter valid email address")
-      : window.alert("Enter your email address");
-
-    info.push(data);
-
-    subscribeForm.reset();
-  });
 
 if (submitBtn)
   submitBtn.addEventListener("click", function (e) {
